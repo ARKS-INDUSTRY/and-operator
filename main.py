658eacbe52259ea7a0daf290c97f7c6982aa5744
Rakesharks <59530718+ARKS-INDUSTRY@users.noreply.github.com>
2021-09-04 13:20:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+a = int(input("enter the value"))
+b = int(input("enter the value"))
+c = int(input("enter the value"))
+if a > b and a > c:
+  print("a is greater")
+if b > a and b > c:
+  print("b is greater")
+if c > a and c > b :
+  print("c is  greater")
